a03eff2ab016a93696bc4f24e2af35093bfbd9ce
tanykazy <tanykazy@gmail.com>
2024-02-24 23:23:34 +0900

    Add image loader

--- masky/editor-component.js
+++ masky/editor-component.js
@@ -2,18 +2,240 @@ import {
   LitElement,
   html,
   css,
-} from "https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js";
+  styleMap,
+} from "https://cdn.jsdelivr.net/gh/lit/dist@3/all/lit-all.min.js";
 
 class EditorComponent extends LitElement {
   static styles = css`
+    #canvas-wrapper {
+      position: relative;
+      overflow: hidden;
+      max-width: 100%;
+      /* width: 100%; */
+      /* height: 80vh; */
+      padding: 0;
+      box-sizing: content-box;
+    }
+    #canvas-wrapper::before {
+      content: "";
+      display: block;
+      padding-top: 50%;
+    }
+    #canvas-wrapper canvas {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      margin: auto;
+      border: 0;
+      max-width: 100%;
+      box-sizing: content-box;
+      padding: 0;
+      /* margin: 0; */
+    }
   `;
 
   render() {
+    const styles = {
+      ...this.styles,
+      "transform-origin": `${this.xpoint}px ${this.ypoint}px`,
+      transform: `scale(${this.zoomRario})`,
+    };
     return html`
-      <canvas width="300" height="300">
-        キャンバスの表示内容を説明する代替テキストです。
-      </canvas>
+      <div id="editor-wrapper">
+        <canvas
+          id="image-canvas"
+          class="editor-canvas"
+          style=${styleMap(styles)}
+          @mousedown="${this.startPoint}"
+          @mousemove="${this.movePoint}"
+          @mouseup="${this.endPoint}"
+          @wheel="${this.mouseWheel}"
+        >
+        </canvas>
+        <input
+          id="image-input"
+          type="file"
+          accept="image/*"
+          @input="${this.onInputImageFile}"
+        />
+      </div>
     `;
+  }
+
+  static properties = {
+    // width: {},
+    // height: {},
+    moveflg: {},
+    xpoint: {},
+    ypoint: {},
+    zoomRario: {},
+  };
+
+  constructor() {
+    super();
+    this.moveflg = false;
+    this.xpoint = 0;
+    this.ypoint = 0;
+    this.zoomRario = 1;
+  }
+
+  onInputImageFile(event) {
+    const input = this.renderRoot.getElementById("image-input");
+    // console.log(input);
+    // console.log(input.value);
+    // console.log(input.files);
+    const file = input.files[0];
+    console.log(file);
+
+    const fileReader = new FileReader();
+    fileReader.onload = (event) => {
+      this.imageLoader(event.target.result);
+    };
+    fileReader.readAsDataURL(file);
+  }
+
+  // マウスホイール変更イベント
+  mouseWheel(event) {
+    console.log(event);
+
+    this.xpoint = event.clientX;
+    this.ypoint = event.clientY;
+
+    // 拡大率算出
+    const temp = event.deltaY < 0 ? 1 : -1;
+    this.zoomRario += 0.1 * temp;
+
+    // 拡大率は1～5まで
+    if (this.zoomRario < 1) {
+      this.zoomRario = 1;
+    } else if (this.zoomRario > 10) {
+      this.zoomRario = 10;
+    }
+
+    // 小数点第二以下切り捨て
+    this.zoomRario = Math.round(this.zoomRario * 10) / 10;
+
+    console.log(this.zoomRario);
+
+    // 算出した拡大率で描画
+    // zoom();
+    // document.getElementById("dispScale").innerHTML = this.zoomRario;
+
+    // ポインタも再設定
+    // if (
+    //   (mode == "1" && (inputType == "1" || inputType == "2")) ||
+    //   mode == "2"
+    // ) {
+    //   pointer(event);
+    // }
+  }
+
+  // // 拡大縮小処理
+  // zoom() {
+  //   // 拡大縮小の起点を設定
+  //   $("#imageCanvas").css({
+  //     "transform-origin":
+  //       document.getElementById("dispX").innerHTML +
+  //       "px " +
+  //       document.getElementById("dispY").innerHTML +
+  //       "px",
+  //   });
+  //   $("#drawCanvas").css({
+  //     "transform-origin":
+  //       document.getElementById("dispX").innerHTML +
+  //       "px " +
+  //       document.getElementById("dispY").innerHTML +
+  //       "px",
+  //   });
+  //   $("#drawTempCanvas").css({
+  //     "transform-origin":
+  //       document.getElementById("dispX").innerHTML +
+  //       "px " +
+  //       document.getElementById("dispY").innerHTML +
+  //       "px",
+  //   });
+  //   $("#pointerCanvas").css({
+  //     "transform-origin":
+  //       document.getElementById("dispX").innerHTML +
+  //       "px " +
+  //       document.getElementById("dispY").innerHTML +
+  //       "px",
+  //   });
+
+  //   // 拡大縮小
+  //   $("#imageCanvas").css({ transform: "scale(" + zoomRario + ")" });
+  //   $("#drawCanvas").css({ transform: "scale(" + zoomRario + ")" });
+  //   $("#drawTempCanvas").css({ transform: "scale(" + zoomRario + ")" });
+  //   $("#pointerCanvas").css({ transform: "scale(" + zoomRario + ")" });
+  // }
+
+  imageLoader(url) {
+    const canvas = this.renderRoot.getElementById("image-canvas");
+    const context = canvas.getContext("2d");
+    const image = new Image();
+    image.onload = function () {
+      const height = 240;
+      canvas.width = (image.naturalWidth * height) / image.naturalHeight;
+      canvas.height = height;
+      // canvas.width = image.naturalWidth;
+      // canvas.height = image.naturalHeight;
+      context.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+      // console.log(canvas);
+    };
+    image.src = url;
+  }
+
+  startPoint(event) {
+    const canvas = this.renderRoot.getElementById("image-canvas");
+    const context = canvas.getContext("2d");
+
+    event.preventDefault();
+
+    context.beginPath();
+
+    // 矢印の先っぽから始まるように調整
+    this.xpoint = event.clientX;
+    this.ypoint = event.clientY;
+
+    context.moveTo(this.xpoint, this.ypoint);
+  }
+
+  movePoint(event) {
+    console.log(event);
+    if (event.buttons === 1 || event.witch === 1 || event.type == "touchmove") {
+      const canvas = this.renderRoot.getElementById("image-canvas");
+      const context = canvas.getContext("2d");
+
+      this.xpoint = event.clientX;
+      this.ypoint = event.clientY;
+      this.moveflg = true;
+
+      const defSize = 2;
+      const defColor = "#ff0000";
+      context.lineTo(this.xpoint, this.ypoint);
+      context.lineCap = "round";
+      context.lineWidth = defSize * 2;
+      context.strokeStyle = defColor;
+      context.stroke();
+    }
+  }
+
+  endPoint(event) {
+    if (this.moveflg === false) {
+      const canvas = this.renderRoot.getElementById("image-canvas");
+      const context = canvas.getContext("2d");
+      const defSize = 2;
+      const defColor = "#ff0000";
+      context.lineTo(this.xpoint - 1, this.ypoint - 1);
+      context.lineCap = "round";
+      context.lineWidth = defSize * 2;
+      context.strokeStyle = defColor;
+      context.stroke();
+    }
+    this.moveflg = false;
   }
 }
 customElements.define("editor-component", EditorComponent);
